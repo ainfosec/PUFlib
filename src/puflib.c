339664e43a9ef4fc77a77133facd0207fbda4b07
Chris Pavlina <pavlinac@ainfosec.com>
2016-06-22 17:13:14 -0400

    Remove POSIX includes from puflib.c
    Signed-off-by: Chris Pavlina <pavlinac@ainfosec.com>

--- src/puflib.c
+++ src/puflib.c
@@ -8,11 +8,6 @@
 #include "platform.h"
 
 #include <string.h>
-#include <limits.h>
-#include <unistd.h>
-#include <sys/types.h>
-#include <sys/stat.h>
-#include <fcntl.h>
 #include <errno.h>
 
 extern module_info const * const PUFLIB_MODULES[];
